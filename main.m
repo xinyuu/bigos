0c0fd01b015d3bb57a1d35082b787098c148786c
Noah Evans <noah.evans@gmail.com>
2015-04-18 14:12:10 -0600

    added a main script that can orchestrate our project, this could also be our report

---
+++ main.m
@@ -0,0 +1,11 @@
+%% Reading the data
+
+% read all files from the training directory
+
+%% Feature extraction
+
+%% Cross validated training on the extracted features
+
+%% Testing our model on the training data
+
+
