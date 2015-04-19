68d4f947e7d535fc2ebc09264e10ac1e0b80ccff
Noah Evans <noah.evans@gmail.com>
2015-04-19 12:41:13 -0600

    added some code to separate out the training data by class, then run a transform on the first five elements of that class

--- main.m
+++ main.m
@@ -6,10 +6,31 @@
 
 % if your matlab is not in the project directory you will need to
 % cd 'project/dir'
-training = dir('rhino/train')
+training = dir('rhino/train');
+
+labels = readtable('rhino/trainLabels.csv');
+level = cell(5,1);
+for i = 0:4
+    level{i+1} = labels(labels.level == i,:);
+    % strcat filenames with.jpeg
+end
+
+for i = 1:4
+    for j = 1:5
+        [f] = level{i}{j,1}{1};
+        fin = sprintf('rhino/train/%s.jpeg', f);
+        fout = sprintf('rhino/trainfeat/%s_wv_%d.jpeg', f,i);
+        I = imread(fin);
+        [ca1, eh1, ev1, ed1] = waveletanderode(I);
+        imwrite(eh1,fout, 'JPG');
+    end
+end
+ 
 % now we need to know the data format.
 
 %% Feature extraction
+
+do 20 sample images
 I1 = imread('16_left.jpeg');
 I2 = imread('17_left.jpeg');
 
@@ -29,6 +50,8 @@ imwrite(ev2,'17_left_v.jpeg', 'JPG');
 figure, imshow(ed2);
 imwrite(ed2,'17_left_d.jpeg', 'JPG');
 % the main work goes here.
+
+% so let's get 20 values. 
 
 
 
